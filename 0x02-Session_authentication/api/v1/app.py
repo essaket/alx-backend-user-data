34fac6fea69d6b5ed38d84d4b67cd7801fb89113
essaket <hichamessaket17@gmail.com>
2024-09-05 08:45:19 -0700

    Writing task 0

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Route module for the API.
+"""0. Et moi et moi et moi!
 """
 import os
 from os import getenv
@@ -33,29 +33,25 @@ if auth_type == 'session_db_auth':
 
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """Not found handler.
-    """
+    """Not found handler"""
     return jsonify({"error": "Not found"}), 404
 
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
-    """Unauthorized handler.
-    """
+    """Unauthorized handler"""
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
 def forbidden(error) -> str:
-    """Forbidden handler.
-    """
+    """Forbidden handler"""
     return jsonify({"error": "Forbidden"}), 403
 
 
 @app.before_request
 def authenticate_user():
-    """Authenticates a user before processing a request.
-    """
+    """Authenticates a user before processing a request"""
     if auth:
         excluded_paths = [
             "/api/v1/status/",
