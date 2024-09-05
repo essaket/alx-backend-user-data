1649f6f965310c19fab5bd42d4e88fbba2d70d07
essaket <hichamessaket17@gmail.com>
2024-09-05 08:55:45 -0700

    Writing task 4

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Authentication module for the API.
+"""4. Session cookie
 """
 import os
 import re
@@ -8,11 +8,9 @@ from flask import request
 
 
 class Auth:
-    """Authentication class.
-    """
+    """Authentication class"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """Checks if a path requires authentication.
-        """
+        """Checks if a path requires authentication"""
         if path is not None and excluded_paths is not None:
             for exclusion_path in map(lambda x: x.strip(), excluded_paths):
                 pattern = ''
@@ -27,20 +25,17 @@ class Auth:
         return True
 
     def authorization_header(self, request=None) -> str:
-        """Gets the authorization header field from the request.
-        """
+        """Gets the authorization header field from the request"""
         if request is not None:
             return request.headers.get('Authorization', None)
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """Gets the current user from the request.
-        """
+        """Gets the current user from the request"""
         return None
 
     def session_cookie(self, request=None) -> str:
-        """Gets the value of the cookie named SESSION_NAME.
-        """
+        """Gets the value of the cookie named SESSION_NAME"""
         if request is not None:
             cookie_name = os.getenv('SESSION_NAME')
             return request.cookies.get(cookie_name)
