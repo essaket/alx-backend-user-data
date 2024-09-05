991225c80dbf419ee5bd636fa7a874a463201c0a
essaket <hichamessaket17@gmail.com>
2024-09-05 08:50:34 -0700

    Writing task 1

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Session authentication module for the API.
+"""1. Empty session
 """
 from uuid import uuid4
 from flask import request
@@ -9,13 +9,11 @@ from models.user import User
 
 
 class SessionAuth(Auth):
-    """Session authentication class.
-    """
+    """Session authentication class"""
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
-        """Creates a session id for the user.
-        """
+        """Creates a session id for the user"""
         if type(user_id) is str:
             session_id = str(uuid4())
             self.user_id_by_session_id[session_id] = user_id
@@ -23,20 +21,17 @@ class SessionAuth(Auth):
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
         """Retrieves the user id of the user associated with
-        a given session id.
-        """
+        a given session id"""
         if type(session_id) is str:
             return self.user_id_by_session_id.get(session_id)
 
     def current_user(self, request=None) -> User:
-        """Retrieves the user associated with the request.
-        """
+        """Retrieves the user associated with the request"""
         user_id = self.user_id_for_session_id(self.session_cookie(request))
         return User.get(user_id)
 
     def destroy_session(self, request=None):
-        """Destroys an authenticated session.
-        """
+        """Destroys an authenticated session"""
         session_id = self.session_cookie(request)
         user_id = self.user_id_for_session_id(session_id)
         if (request is None or session_id is None) or user_id is None:
