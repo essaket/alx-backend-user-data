e85adfedca9162d1d6d9ce5c32d9ecc5494cc048
essaket <hichamessaket17@gmail.com>
2024-09-11 20:09:16 +0100

    Writing task 8

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """4. Hash password
    5. Register user
+   8. Credentials validation
 """
 from typing import Union
 import uuid
@@ -27,3 +28,14 @@ class Auth:
             raise ValueError(f"User {email} already exists")
         except NoResultFound:
             return self._db.add_user(email, _hash_password(password))
+    
+     def valid_login(self, email: str, password: str) -> bool:
+        """Validate login
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(
+                password.encode('utf-8'),
+                user.hashed_password.encode('utf-8'))
+        except NoResultFound:
+            return False
